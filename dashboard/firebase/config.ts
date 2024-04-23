2c1ee3e313ede1437f90d6d729f186746787f08b
rishikeshydv <rishikeshadh4@gmail.com>
2024-04-23 17:15:45 -0400

    feat: firebase credentials updated

--- dashboard/firebase/config.ts
+++ dashboard/firebase/config.ts
@@ -7,13 +7,15 @@ import dotenv from 'dotenv';
 dotenv.config(); 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjf4Il61JXZYqEVcdU9Rs2xEnC2z-txqA",
-  authDomain: "tapdropdashboard.firebaseapp.com",
-  projectId: "tapdropdashboard",
-  storageBucket: "tapdropdashboard.appspot.com",
-  messagingSenderId: "75892653275",
-  appId: "1:75892653275:web:1662afc6b0d01edac3e0fe",
-  measurementId: "G-L1F3FY9EV5"
+
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
+
 };
 
 
